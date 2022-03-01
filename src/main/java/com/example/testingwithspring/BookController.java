94b8acf3228a614b07e22ea627e245741957a8e2
Marcos Mtz <marcos.the.silencer@gmail.com>
2022-03-01 16:44:52 -0600

    First implementation of BookController with its respective test case.

---
+++ src/main/java/com/example/testingwithspring/BookController.java
@@ -0,0 +1,5 @@
+package com.example.testingwithspring;
+
+public class BookController {
+
+}
---
+++ src/test/java/com/example/testingwithspring/BookControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.testingwithspring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
